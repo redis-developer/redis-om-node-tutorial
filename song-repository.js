dda2f20b9844a414b05ee458796825263e1da15a
Guy Royse <guy@guyroyse.com>
2023-06-15 16:04:33 -0400

    whitespace

--- song-repository.js
+++ song-repository.js
@@ -5,7 +5,7 @@ class Song extends Entity {}
 let schema = new Schema(Song, {
   title: { type: 'string' },     // the title of the song
   artist: { type: 'string' },    // who performed the song
-  genres: { type: 'string[]' },     // array of strings for the genres of the song
+  genres: { type: 'string[]' },  // array of strings for the genres of the song
   lyrics: { type: 'text' },      // the full lyrics of the song
   music: { type: 'text' },       // who wrote the music for the song
   year: { type: 'number' },      // the year the song was releases
